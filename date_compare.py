jbowen102/idevice_media_offload

commit ec70d383c40e11a7f15cb3736eff9a317354bb0c

Change import style for all.

date_compare.py
@@ -1,12 +1,6 @@
-import PIL.Image
-from PIL.ExifTags import TAGS
-from os import listdir, rename
-from os.path import getmtime, isdir
-from os.path import exists as path_exists
-from os.path import splitext as path_splitext
-from os.path import basename as path_basename
-from os.path import dirname as path_dirname
-from time import localtime, struct_time, strftime, strptime
+import PIL
+import os
+import time
 import exiftool
 
 
@@ -26,26 +20,26 @@ def list_all_img_dates(path, rename_with_datestamp=False):
     as they are processed.
     datestamp_all() function below now preferred."""
 
-    if not path_exists(path):
+    if not os.path.exists(path):
         print("Not a valid path.")
         return None
-    elif isdir(path):
+    elif os.path.isdir(path):
         if path[-1] != '/':
             path += '/'
-        image_list = listdir(path)
+        image_list = os.listdir(path)
         image_list.sort()
     else:
         # If path is only a single image instead of a directory, separate out
         # the image name from the path to conform to path + img convention in
         # rest of code. Create a length-one list with that image name to
         # loop through.
-        single_image = path_basename(path) # single-img path has no trailing slash
-        path = path_dirname(path) + "/"
+        single_image = os.path.basename(path) # single-img path has no trailing slash
+        path = os.path.dirname(path) + "/"
         image_list = [single_image]
 
     for img in image_list:
-        img_ext = path_splitext(img)[-1].upper()
-        file_mod_time = strftime(DATETIME_FORMAT, localtime(getmtime(path + img)))
+        img_ext = os.path.splitext(img)[-1].upper()
+        file_mod_time = time.strftime(DATETIME_FORMAT, time.localtime(os.path.getmtime(path + img)))
 
         if img_ext in [".JPG", ".JPEG"]:
             img_obj = PIL.Image.open(path + img)
@@ -53,7 +47,7 @@ def list_all_img_dates(path, rename_with_datestamp=False):
             encoded_exif = img_obj._getexif()
             pil_metadata = {}
             for tag, value in encoded_exif.items():
-                 decoded_key = TAGS.get(tag, tag)
+                 decoded_key = PIL.ExifTags.TAGS.get(tag, tag)
                  if "DateTime" in str(decoded_key):
                      pil_metadata[decoded_key] = value
 
@@ -216,14 +210,14 @@ def datestamp_all(dir_path, longstamp=False):
     terminal output. Uses add_datestamp() function below for each file processed.
     Second parameter determines if date only or both date/time will be added."""
 
-    if not path_exists(dir_path) or not isdir(dir_path):
+    if not os.path.exists(dir_path) or not os.path.isdir(dir_path):
         print("Not a valid directory path.")
         return None
 
     if dir_path[-1] != '/':
         dir_path += '/'
 
-    image_list = listdir(dir_path)
+    image_list = os.listdir(dir_path)
     image_list.sort()
 
     for img in image_list:
@@ -238,18 +232,18 @@ def add_datestamp(img_path, long_stamp=False):
     # need a variable name that stores images best guess-time. look at
     # get_img_date end code.
 
-    if not path_exists(img_path):
+    if not os.path.exists(img_path):
         raise DirectoryNameError("Invalid path passed to add_datestamp() function.")
 
     # Separate out image name from directory path
-    img_name = path_basename(img_path)  # no trailing slash present
-    dir_path = path_dirname(img_path) + "/"
+    img_name = os.path.basename(img_path)  # no trailing slash present
+    dir_path = os.path.dirname(img_path) + "/"
 
     datestamp_obj = get_img_date(img_path)
 
     if datestamp_obj:
-        datestamp_short = strftime('%Y-%m-%d', datestamp_obj)
-        datestamp_long = strftime(DATETIME_FORMAT, datestamp_obj)
+        datestamp_short = time.strftime('%Y-%m-%d', datestamp_obj)
+        datestamp_long = time.strftime(DATETIME_FORMAT, datestamp_obj)
     else:
         # if get_img_date returned None (because file wasn't a recognized img format),
         # don't proceed further.
@@ -287,14 +281,14 @@ def add_datestamp(img_path, long_stamp=False):
 def safe_rename(img_path, new_img_name):
     """Ensures that rename action does not overwrite existing img in target dir."""
 
-    if not path_exists(img_path):
+    if not os.path.exists(img_path):
         raise DirectoryNameError("Invalid path passed to safe_rename() function.")
 
-    target_dir = path_dirname(img_path)
-    target_dir_imgs = listdir(target_dir)
+    target_dir = os.path.dirname(img_path)
+    target_dir_imgs = os.listdir(target_dir)
 
-    new_img_name_noext = path_splitext(new_img_name)[0]
-    img_ext = path_splitext(new_img_name)[-1]
+    new_img_name_noext = os.path.splitext(new_img_name)[0]
+    img_ext = os.path.splitext(new_img_name)[-1]
 
     if new_img_name in target_dir_imgs:
         n = 1
@@ -310,7 +304,7 @@ def safe_rename(img_path, new_img_name):
 
         new_img_name = new_img_name_noext + img_ext
 
-    rename(img_path, target_dir + "/" + new_img_name)
+    os.rename(img_path, target_dir + "/" + new_img_name)
 
 
 def get_img_date(img_path):
@@ -319,13 +313,13 @@ def get_img_date(img_path):
     Returns a struct_time object."""
     # modify to look for each metadata type and fall back on mtime if needed.
 
-    if not path_exists(img_path):
+    if not os.path.exists(img_path):
         raise DirectoryNameError("Invalid path passed to get_img_date() function.")
 
-    img_name = path_basename(img_path)  # no trailing slash in path
-    img_ext = path_splitext(img_name)[-1].upper()
+    img_name = os.path.basename(img_path)  # no trailing slash in path
+    img_ext = os.path.splitext(img_name)[-1].upper()
 
-    if isdir(img_path):
+    if os.path.isdir(img_path):
         print("%s is a directory. Skipping"
                                     % img_name)
         return None
@@ -382,18 +376,18 @@ def get_img_date(img_path):
             return None
 
         if create_time:
-            create_time_obj = strptime(create_time, format)
+            create_time_obj = time.strptime(create_time, format)
             return create_time_obj
         else:
             # Fall back on fs mod time if more precise metadata unavailable.
-            print("Falling back on fs mod time for %s" % path_basename(img_path))
-            file_mod_time_obj = localtime(getmtime(img_path))
+            print("Falling back on fs mod time for %s" % os.path.basename(img_path))
+            file_mod_time_obj = time.localtime(os.path.getmtime(img_path))
             return file_mod_time_obj
 
 
 def tz_adjust(time_str, format, shift):
     """Function to adjust timezone of a datestamp."""
-    time_obj = strptime(time_str, format)
+    time_obj = time.strptime(time_str, format)
 
     ts_list = list(time_obj)
     if ts_list[3] <= shift:
@@ -401,19 +395,19 @@ def tz_adjust(time_str, format, shift):
         return None
     else:
         ts_list[3] -= shift         # EST. DST not accounted for.
-        time_obj_adjusted = struct_time(tuple(ts_list))
-        time_str_adjusted = strftime(format, time_obj_adjusted)
+        time_obj_adjusted = time.struct_time(tuple(ts_list))
+        time_str_adjusted = time.strftime(format, time_obj_adjusted)
         return time_str_adjusted
 
 
 def meta_dump(img_path):
     """Display all available exiftool data (for any file w/ EXIF data)."""
 
-    if not path_exists(img_path):
+    if not os.path.exists(img_path):
         print("Not a valid path.")
         return None
-    elif isdir(img_path):
-        print("%s is a directory. Need path to image." % path_basename(img_path))
+    elif os.path.isdir(img_path):
+        print("%s is a directory. Need path to image." % os.path.basename(img_path))
         return None
 
     with exiftool.ExifTool() as et:

date_organize_tool.py
@@ -1,9 +1,6 @@
-from os import listdir, mkdir, rmdir
-from os.path import exists as path_exists
-from os.path import basename as path_basename   # returns file or dir at end of path
-from shutil import copy2 as sh_copy2
-from shutil import copytree as sh_copytree
-from time import strftime, strptime
+import os
+import shutil
+import time
 from tqdm import tqdm
 
 from pic_offload_tool import RawOffloadGroup
@@ -28,7 +25,7 @@ class OrganizedGroup(object):
         self.date_root_path = self.bu_root_path + "Organized/"
         self.buffer_root_path = buffer_root
         # Double-check Organized folder is there.
-        if not path_exists(self.date_root_path):
+        if not os.path.exists(self.date_root_path):
             raise OrganizeFolderError("Organized dir not found at %s! "
                         "Pics not organized. Terminating" % self.date_root_path)
         # Initialize object dictionary. Instantiate most recent year object.
@@ -47,7 +44,7 @@ class OrganizedGroup(object):
 
     def get_yr_list(self):
         # Refresh date_root_path every time in case dir changes.
-        year_list = listdir(self.get_root_path())
+        year_list = os.listdir(self.get_root_path())
         year_list.sort()
         return year_list
 
@@ -92,12 +89,12 @@ class OrganizedGroup(object):
             print("Attempt to pull image %s into folder %s-%s, "
                                     "but that is older than one month, so "
                                     "timestamp is likely wrong." %
-                                    (path_basename(img_orig_path), yr_str, mo_str))
+                                    (os.path.basename(img_orig_path), yr_str, mo_str))
             man_img_time = input("Manually specify timestamp in "
                                 "YYYY-MM-DD format.\n>>>")
             if man_img_time:
                 try:
-                    man_img_time_struct = strptime(man_img_time, "%Y-%m-%d")
+                    man_img_time_struct = time.strptime(man_img_time, "%Y-%m-%d")
                     self.insert_img(img_orig_path, man_img_time_struct)
                 except:
                     print("Bad date format. Try again.\n")
@@ -137,7 +134,7 @@ class YearDir(object):
         self.OrgGroup = OrgGroup
 
         if not self.year_name in OrgGroup.get_yr_list():
-            mkdir(self.year_path)
+            os.mkdir(self.year_path)
         # Instantiate latest mo and put in dict of months
         self.mo_objs = {}
         # Run get_latest_mo in case it hasn't been run yet so latest_mo obj
@@ -148,7 +145,7 @@ class YearDir(object):
         return self.year_path
 
     def get_mo_list(self):
-        mo_list = listdir(self.year_path)
+        mo_list = os.listdir(self.year_path)
         mo_list.sort()
         return mo_list
 
@@ -199,7 +196,7 @@ class YearDir(object):
             # If the image is from an earlier month, then something's wrong.
             print("Attempt to pull image %s into folder %s, "
                         "but that folder is older than one month."
-                        % (path_basename(img_orig_path), yrmon))
+                        % (os.path.basename(img_orig_path), yrmon))
             list_all_img_dates(img_orig_path)
 
     def __str__(self):
@@ -218,37 +215,37 @@ class MoDir(object):
         self.YrDir = YrDir
 
         if not self.dir_name in YrDir.get_mo_list():
-            mkdir(self.yrmonth_path)
+            os.mkdir(self.yrmonth_path)
 
     def get_mo_path(self):
         return self.yrmonth_path
 
     def get_img_list(self):
-        self.img_list = listdir(self.yrmonth_path)
+        self.img_list = os.listdir(self.yrmonth_path)
         return self.img_list
 
     def insert_img(self, img_orig_path, img_time):
         # make sure image not already here
-        img_name = path_basename(img_orig_path)   # no trailing slash
+        img_name = os.path.basename(img_orig_path)   # no trailing slash
         if img_name in self.get_img_list():
             print("Attempt to pull image %s into folder %s, "
                                 "but an image of that name already exists here."
                                                     % (img_name, self.dir_name))
         else:
-            stamped_name = strftime('%Y-%m-%d', img_time) + '_' + img_name
+            stamped_name = time.strftime('%Y-%m-%d', img_time) + '_' + img_name
             img_new_path = self.yrmonth_path + stamped_name
 
             # Copy into the dated directory
-            sh_copy2(img_orig_path, img_new_path)
+            shutil.copy2(img_orig_path, img_new_path)
 
             # Also copy the img into the cat buffer for next step in prog.
-            if ".AAE" not in path_basename(img_orig_path):
+            if ".AAE" not in os.path.basename(img_orig_path):
                 # Don't copy AAE files into cat buffer.
                 # They will still exist in raw and organized folders, but it doesn't serve
                 # any value to copy them elsewhere.
                 # They can also have dates that don't match the corresponding img/vid.
                 # This can cause confusion.
-                sh_copy2(img_orig_path, self.YrDir.OrgGroup.get_buffer_root_path() + stamped_name)
+                shutil.copy2(img_orig_path, self.YrDir.OrgGroup.get_buffer_root_path() + stamped_name)
 
     def __str__(self):
         return self.dir_name

pic_categorize_tool.py
@@ -3,7 +3,7 @@ import shutil
 import time
 from tqdm import tqdm
 import subprocess
-from hashlib import sha1
+import hashlib
 
 from dir_names import CAT_DIRS
 
@@ -280,11 +280,11 @@ def display_photo(img_path):
 
 def same_hash(img1_path, img2_path):
     with open(img1_path, 'rb') as file_obj:
-        img1_hash = sha1(file_obj.read())
+        img1_hash = hashlib.sha1(file_obj.read())
         # print(img1_hash.hexdigest())
 
     with open(img2_path, 'rb') as file_obj:
-        img2_hash = sha1(file_obj.read())
+        img2_hash = hashlib.sha1(file_obj.read())
         # print(img2_hash.hexdigest())
 
     if img1_hash.hexdigest() == img2_hash.hexdigest():

pic_offload_tool.py
@@ -1,12 +1,9 @@
 # https://docs.python.org/3/library/time.html
-from os import listdir, mkdir, rmdir
-from os.path import getsize, isfile
-from os.path import exists as path_exists
-from shutil import copy2 as sh_copy2
-from shutil import move as sh_move
-from shutil import copytree as sh_copytree
-from time import strftime, strptime
+import os
+import shutil
+import time
 from tqdm import tqdm
+
 from dir_names import IPHONE_DCIM_PREFIX
 
 
@@ -39,14 +36,14 @@ class iPhoneDCIM(object):
     def find_root(self):
         # Look at all gvfs handles to find one having name starting w/ "gphoto".
         # There should only be one.
-        gvfs_handles = listdir(IPHONE_DCIM_PREFIX)
+        gvfs_handles = os.listdir(IPHONE_DCIM_PREFIX)
         count = 0
         for i, handle in enumerate(gvfs_handles):
             if handle[0:6] == 'gphoto':
                 iphone_handle = handle
                 count += 1
 
-        if count == 0 or not listdir(IPHONE_DCIM_PREFIX + iphone_handle):
+        if count == 0 or not os.listdir(IPHONE_DCIM_PREFIX + iphone_handle):
             input("Error: Can't find iOS device in %s\nPress Enter to try again." % IPHONE_DCIM_PREFIX)
             self.find_root()
         elif count > 1:
@@ -56,7 +53,7 @@ class iPhoneDCIM(object):
         else:
             # Found exactly one "gphoto" folder
             self.DCIM_path = IPHONE_DCIM_PREFIX + iphone_handle + "/DCIM/"
-            self.APPLE_folders = listdir(self.DCIM_path)
+            self.APPLE_folders = os.listdir(self.DCIM_path)
             if not self.APPLE_folders:
                 # Empty DCIM folder indicates temporary issue like locked device.
                 os_error_response = input("\nCan't access device pictures.\n"
@@ -87,7 +84,7 @@ class iPhoneDCIM(object):
 
     def APPLE_contents(self, APPLE_folder_name):
         # Exception handling done by APPLE_folder_path() method
-        APPLE_contents = listdir(self.APPLE_folder_path(APPLE_folder_name))
+        APPLE_contents = os.listdir(self.APPLE_folder_path(APPLE_folder_name))
         APPLE_contents.sort()
         return APPLE_contents
 
@@ -118,7 +115,7 @@ class RawOffloadGroup(object):
         self.RO_root_path = self.bu_root_path + "Raw_Offload/"
 
         # Double-check Raw_Offload folder is there.
-        if not path_exists(self.RO_root_path):
+        if not os.path.exists(self.RO_root_path):
             raise RawOffloadError("Raw_Offload dir not found at %s! "
                         "Pics not offloaded. Terminating" % self.RO_root_path)
 
@@ -141,7 +138,7 @@ class RawOffloadGroup(object):
 
     def generate_offload_list(self):
         # Create list that contains all raw-offload folder names.
-        RO_root_contents = listdir(self.RO_root_path)
+        RO_root_contents = os.listdir(self.RO_root_path)
         RO_root_contents.sort()
         self.offload_list = RO_root_contents
 
@@ -168,12 +165,12 @@ class RawOffloadGroup(object):
         return self.get_latest_offload_obj().list_APPLE_folders()[-1]
 
     def remove_bad_dir_items(self):
-        if isfile(self.get_RO_root() + self.get_last_offload_name()):
+        if os.path.isfile(self.get_RO_root() + self.get_last_offload_name()):
             input("File found where only offload folders should be in RO root.\n"
             "Press Enter to try again.\n>>> ")
             # try again
             self.remove_bad_dir_items()
-        elif not listdir(self.get_RO_root() + self.get_last_offload_name()):
+        elif not os.listdir(self.get_RO_root() + self.get_last_offload_name()):
             delete_empty_ro = input("Folder %s in raw_offload directory is empty, "
             "probably from previous aborted offload.\n"
             "Press 'd' to delete folder and retry operation.\n"
@@ -181,7 +178,7 @@ class RawOffloadGroup(object):
 
             if delete_empty_ro == 'd':
                 # Delete that folder name from list attribute
-                rmdir(self.get_RO_root() + self.get_last_offload_name())
+                os.rmdir(self.get_RO_root() + self.get_last_offload_name())
 
                 # re-generate offload list after deleting an element
                 self.generate_offload_list()
@@ -204,7 +201,7 @@ class RawOffloadGroup(object):
 
         # Check all other offload folders for the overlap folder
         for offload in self.offload_list[:-1]:
-            if overlap_folder in listdir(self.get_RO_root() + offload):
+            if overlap_folder in os.listdir(self.get_RO_root() + offload):
                 # Make RawOffload object for each offload containing overlap
                 # folder, and add them to the list.
                 PrevOL = RawOffload(offload, self)
@@ -213,13 +210,13 @@ class RawOffloadGroup(object):
 
     def create_new_offload(self):
         # Pass in current timestamp as the new offload's name
-        new_timestamp = strftime(DATETIME_FORMAT)
+        new_timestamp = time.strftime(DATETIME_FORMAT)
         NewOffload = NewRawOffload(new_timestamp, self)
         self.merge_todays_offloads()
         return NewOffload
 
     def merge_todays_offloads(self):
-        today = strftime("%Y-%m-%d")
+        today = time.strftime("%Y-%m-%d")
         todays_offloads = []
 
         # Have to refresh offload list. Doesn't yet contain new offload folder
@@ -259,15 +256,15 @@ class RawOffloadGroup(object):
             for APPLE_folder in SrcFolder.list_APPLE_folders():
                 # If the folder doesn't exist in the destination folder yet, create it.
                 if not APPLE_folder in DestFolder.list_APPLE_folders():
-                    mkdir(DestFolder.get_full_path() + APPLE_folder)
+                    os.mkdir(DestFolder.get_full_path() + APPLE_folder)
 
                 for image in SrcFolder.APPLE_contents(APPLE_folder):
-                    sh_move(SrcFolder.APPLE_folder_path(APPLE_folder) + image,
+                    shutil.move(SrcFolder.APPLE_folder_path(APPLE_folder) + image,
                             DestFolder.APPLE_folder_path(APPLE_folder))
                 # Delete each APPLE directory after copying everything out of it
-                rmdir(SrcFolder.APPLE_folder_path(APPLE_folder))
+                os.rmdir(SrcFolder.APPLE_folder_path(APPLE_folder))
             # Delete each RO directory after copying everything out of it
-            rmdir(SrcFolder.get_full_path())
+            os.rmdir(SrcFolder.get_full_path())
 
     def __str__(self):
         return self.get_RO_root()
@@ -297,12 +294,12 @@ class RawOffload(object):
 
     def list_APPLE_folders(self):
         # Sorted; not full paths
-        APPLE_folders = listdir(self.get_full_path())
+        APPLE_folders = os.listdir(self.get_full_path())
         APPLE_folders.sort()
         return APPLE_folders
 
     def newest_APPLE_folder(self):
-        if isfile(self.list_APPLE_folders()[-1]):
+        if os.path.isfile(self.list_APPLE_folders()[-1]):
             raise DirectoryNameError("File found where only APPLE folders should "
             "be in %s. Cannot determine newest APPLE folder." % self.full_path)
         else:
@@ -317,7 +314,7 @@ class RawOffload(object):
 
     def APPLE_contents(self, APPLE_folder_name):
         # Exception handling done by APPLE_folder_path() method
-        APPLE_contents = listdir(self.APPLE_folder_path(APPLE_folder_name))
+        APPLE_contents = os.listdir(self.APPLE_folder_path(APPLE_folder_name))
         APPLE_contents.sort()
         return APPLE_contents
 
@@ -325,7 +322,7 @@ class RawOffload(object):
         return self.offload_dir_name
 
     def get_timestamp_struct(self):
-        return strptime(self.offload_dir_name, DATETIME_FORMAT)
+        return time.strptime(self.offload_dir_name, DATETIME_FORMAT)
 
     def __str__(self):
         return self.full_path
@@ -353,13 +350,13 @@ class NewRawOffload(RawOffload):
         # Create new directory w/ today's date/time stamp in Raw_Offload.
         self.offload_dir_name = offload_name
         self.full_path = (self.Parent.get_RO_root() + self.offload_dir_name + '/')
-        if path_exists(self.full_path):
+        if os.path.exists(self.full_path):
             # Make sure folder w/ this name (current date/time stamp) doesn't already exist.
             raise RawOffloadError("Tried to create directory at\n%s\nbut that "
                                   "directory already exists. No changes made."
                                                     % self.full_path)
         else:
-            mkdir(self.full_path)
+            os.mkdir(self.full_path)
 
     def run_overlap_offload(self):
         # Find the last (newest) APPLE dir in the most recent offload.
@@ -400,7 +397,7 @@ class NewRawOffload(RawOffload):
 
         # Create a destination folder in the new Raw Offload directory with the same APPLE name.
         self.new_overlap_path = self.full_path + self.overlap_folder + '/'
-        mkdir(self.new_overlap_path)
+        os.mkdir(self.new_overlap_path)
 
         # Iterate through each folder that contains the overlap folder.
         # store the img names in a set for fast membership testing (order not important).
@@ -423,7 +420,7 @@ class NewRawOffload(RawOffload):
                 # Requires iOS restart to fix.
                 while True:
                     try:
-                        sh_copy2(src_img_path, self.new_overlap_path)
+                        shutil.copy2(src_img_path, self.new_overlap_path)
                         break
                     except OSError:
                         os_error_response = input("\nEncountered device I/O error during overlap "
@@ -450,7 +447,7 @@ class NewRawOffload(RawOffload):
         # This would happen in the rare case of the previous offload happening
         # just before the next photo saved starts a new APPLE photo on the device.
         if not self.APPLE_contents(self.overlap_folder):
-            rmdir(self.new_overlap_path)
+            os.rmdir(self.new_overlap_path)
             print("No new pictures contained in %s (overlap folder) since "
                                     "last offload." % self.overlap_folder)
 
@@ -470,15 +467,15 @@ class NewRawOffload(RawOffload):
                 print("New APPLE folder %s found on iPhone - copying." % folder)
                 # Create the new destination folder
                 new_dst_APPLE_path = self.full_path + folder + '/'
-                mkdir(new_dst_APPLE_path)
+                os.mkdir(new_dst_APPLE_path)
 
                 # Loop through source APPLE folder and copy to new dst folder.
-                imgs = listdir(self.src_iPhone_dir.APPLE_folder_path(folder))
+                imgs = os.listdir(self.src_iPhone_dir.APPLE_folder_path(folder))
                 imgs.sort() # Need to sort so if a pic offload fails, you can determine which
                 for img in tqdm(imgs):
                     while True:
                         try:
-                            sh_copy2(self.src_iPhone_dir.APPLE_folder_path(folder) + img,
+                            shutil.copy2(self.src_iPhone_dir.APPLE_folder_path(folder) + img,
                                     new_dst_APPLE_path)
                             break
                         except OSError:

commit db62a8ab5179550952bcd7fa2aff852a5e66bce3

Fix PIL module import issue caused by commit ec70d383c

date_compare.py
@@ -1,4 +1,6 @@
-import PIL
+import PIL.Image
+# https://stackoverflow.com/questions/11911480/python-pil-has-no-attribute-image
+from PIL.ExifTags import TAGS
 import os
 import time
 import exiftool