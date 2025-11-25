e9ae5cd94dcd9123e00c65d82c6c2e635f103507
JB (work) <ew15dro6k216@opayq.net>
2025-11-25 15:23:30 -0500

    Change exiftool object call to conform to WSL version (I guess).

--- date_compare.py
+++ date_compare.py
@@ -63,8 +63,8 @@ def list_all_img_dates(path, skip_unknown=True, rename_with_datestamp=False):
             else:
                 pil_metadata = dict()
 
-            with exiftool.ExifToolHelper() as et:
-                exiftool_metadata = et.get_metadata(img_path)[0]
+            with exiftool.ExifTool() as et:
+                exiftool_metadata = et.get_metadata(img_path)
 
             # "*" indicates metadata most likely to be actual creation time.
             print((img + ":\n"
@@ -88,8 +88,8 @@ def list_all_img_dates(path, skip_unknown=True, rename_with_datestamp=False):
                        )).expandtabs(28))
 
         elif img_ext == ".HEIC":
-            with exiftool.ExifToolHelper() as et:
-                exiftool_metadata = et.get_metadata(img_path)[0]
+            with exiftool.ExifTool() as et:
+                exiftool_metadata = et.get_metadata(img_path)
 
             # "*" indicates metadata most likely to be actual creation time.
             print((img + ":\n"
@@ -108,8 +108,8 @@ def list_all_img_dates(path, skip_unknown=True, rename_with_datestamp=False):
                        )).expandtabs(28))
 
         elif img_ext in [".GIF", ".WEBP"]:
-            with exiftool.ExifToolHelper() as et:
-                metadata = et.get_metadata(img_path)[0]
+            with exiftool.ExifTool() as et:
+                metadata = et.get_metadata(img_path)
 
             # "*" indicates metadata most likely to be actual creation time.
             print((img + ":\n"
@@ -120,8 +120,8 @@ def list_all_img_dates(path, skip_unknown=True, rename_with_datestamp=False):
                            )).expandtabs(28))
 
         elif img_ext == ".MOV":
-            with exiftool.ExifToolHelper() as et:
-                metadata = et.get_metadata(img_path)[0]
+            with exiftool.ExifTool() as et:
+                metadata = et.get_metadata(img_path)
 
             # "*" indicates metadata most likely to be actual creation time.
             print((img + ":\n"
@@ -144,8 +144,8 @@ def list_all_img_dates(path, skip_unknown=True, rename_with_datestamp=False):
                            )).expandtabs(28))
 
         elif img_ext == ".3GP":
-            with exiftool.ExifToolHelper() as et:
-                metadata = et.get_metadata(img_path)[0]
+            with exiftool.ExifTool() as et:
+                metadata = et.get_metadata(img_path)
 
             # "*" indicates metadata most likely to be actual creation time.
             print((img + ":\n"
@@ -168,8 +168,8 @@ def list_all_img_dates(path, skip_unknown=True, rename_with_datestamp=False):
                            )).expandtabs(28))
 
         elif img_ext == ".MP4":
-            with exiftool.ExifToolHelper() as et:
-                metadata = et.get_metadata(img_path)[0]
+            with exiftool.ExifTool() as et:
+                metadata = et.get_metadata(img_path)
 
             # "*" indicates metadata most likely to be actual creation time.
             print((img + ":\n"
@@ -201,8 +201,8 @@ def list_all_img_dates(path, skip_unknown=True, rename_with_datestamp=False):
             else:
                 pil_date_created = None
 
-            with exiftool.ExifToolHelper() as et:
-                metadata = et.get_metadata(img_path)[0]
+            with exiftool.ExifTool() as et:
+                metadata = et.get_metadata(img_path)
 
             # "*" indicates metadata most likely to be actual creation time.
             print((img + ":\n"
@@ -224,8 +224,8 @@ def list_all_img_dates(path, skip_unknown=True, rename_with_datestamp=False):
                 else:
                     adjustmentTimestamp = None
 
-            with exiftool.ExifToolHelper() as et:
-                metadata = et.get_metadata(img_path)[0]
+            with exiftool.ExifTool() as et:
+                metadata = et.get_metadata(img_path)
 
             # "*" indicates metadata most likely to be actual creation time.
             print((img + "\n"
@@ -403,8 +403,8 @@ def get_img_date_plus(img_path, skip_unknown=True):
                                     % img_name)
         return None
 
-    with exiftool.ExifToolHelper() as et:
-        metadata = et.get_metadata(img_path)[0]
+    with exiftool.ExifTool() as et:
+        metadata = et.get_metadata(img_path)
         # Different files have different names for the creation date in the
         # metadata.
         if img_ext in [".JPG", ".JPEG", ".HEIC", ".CR2"]:
@@ -550,8 +550,8 @@ def get_comment(img_path, print_type=False):
         print("Not a valid image path.")
         return None
 
-    with exiftool.ExifToolHelper() as et:
-        metadata = et.get_metadata(img_path)[0]
+    with exiftool.ExifTool() as et:
+        metadata = et.get_metadata(img_path)
 
         exif_img_desc = metadata.get("EXIF:ImageDescription")
         caption_abst = metadata.get("IPTC:Caption-Abstract")
@@ -647,8 +647,8 @@ def meta_dump(img_path):
                                                 % os.path.basename(img_path))
         return None
 
-    with exiftool.ExifToolHelper() as et:
-        metadata = et.get_metadata(img_path)[0]
+    with exiftool.ExifTool() as et:
+        metadata = et.get_metadata(img_path)
         for key, value in metadata.items():
              print(str(key) + ": " + str(value))
 
